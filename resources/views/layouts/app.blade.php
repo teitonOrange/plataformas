f5c73706b6f8bb43603ce522799aa63c2952de11
Andres Prieto <katerinuuu@gmail.com>
2023-10-07 16:40:08 -0300

    Corregidos cambios menores.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,7 +24,7 @@
               </div>
               <div class="hidden sm:ml-6 sm:block">
                 <div class="flex space-x-4">
-                  <a href="{{ url('/') }}" class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Inicio</a>
+                  <a href="{{ url('/') }}" class="hover:bg-gray-700 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Inicio</a>
                   <a href="#" class="text-white hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Proximamente</a>
                   <a href="#" class="text-white hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Próximamente</a>
                   @guest
@@ -45,7 +45,7 @@
                         v-pre>
                         {{ Auth::user()->name }}
                         </a>
-                        <a {{ route('home') }}href="" class="text-white hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Cargar Rutas</a>
+                        <a href="{{ route('home') }}" class="text-white hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Cargar Rutas</a>
                         <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                             <a class="dropdown-item text-white hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium hidden sm:ml-6 sm:block" href="{{ route('logout') }}"
                             onclick="event.preventDefault();
@@ -67,8 +67,7 @@
         @yield('content')
     </main>
 </body>
-
-<footer class="shadow m-4 mx-auto" style="background-color: #333333">
+<footer class="shadow mx-auto" style="background-color: #333333">
     <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div class="sm:flex sm:items-center sm:justify-between">
             <a href="{{ url('/') }}" class="flex items-center mb-4 sm:mb-0">
